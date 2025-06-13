cc1a5e3c6e3d3ed128060f692e86fd6a0bad3100
Huy Vũ <93518371+huyvu23@users.noreply.github.com>
2025-06-13 16:31:03 +0700

    feat: delete and update item todo

--- app/api/v1/endpoints/todo.py
+++ app/api/v1/endpoints/todo.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter,HTTPException
+from fastapi import APIRouter,HTTPException,status
+from fastapi.responses import Response
 from app.schemas.todo import TodoCreate, TodoRead
-from app.crud.todo import create_todo, get_all_todos,get_todo_according_id
+from app.crud.todo import create_todo,delete_todo_item, get_all_todos,get_todo_according_id,update_status_todo
 router = APIRouter()
 
 @router.post("/", response_model=TodoRead)
@@ -13,8 +14,33 @@ def read_all():
 
 @router.get("/{id}", response_model=TodoRead)
 def get_todo_by_id(id:int):
-    result = get_todo_according_id(int(id))
-    if bool(result):
-        return result
-    raise HTTPException(status_code=404, detail=f"Todo with id {id} not found")
+    try:
+        result = get_todo_according_id(int(id))
+        if bool(result):
+            return result
+        raise HTTPException(status_code=404, detail=f"Todo with id {id} not found")
+    except ValueError as e:
+        print("Lỗi:", e)
+
+@router.put("/{id}")
+def update_status_todo_item(id:int,response_model=TodoRead):
+    try:
+        response = update_status_todo(id)
+        if bool(response):
+           return response
+        raise HTTPException(status_code=404, detail=f"Todo with id {id} not found")
+    except ValueError as e:
+        print("Lỗi:", e)
+
+@router.delete("/{id}")
+def delete_to_by_id(id:int):
+    try:
+        response = delete_todo_item(id)
+        if response:
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        raise HTTPException(status_code=404, detail=f"Todo with id {id} not found")
+    except ValueError as e:
+        print("Lỗi:", e)
+
+
 
--- app/crud/todo.py
+++ app/crud/todo.py
@@ -2,6 +2,7 @@ from app.schemas.todo import TodoCreate
 from app.models.todo import Todo
 from app.core.storage import todos
 from typing import Optional
+import json
 
 def create_todo(todo_create: TodoCreate) -> Todo:
     new_id = len(todos) + 1
@@ -21,3 +22,21 @@ def get_todo_according_id(id:int) -> Optional[Todo]:
         if item.id == id:
             return item
     return None
+
+def update_status_todo(id:int) -> Optional[Todo]:
+    item_need_update = None
+    for item in todos:
+        if item.id == id:
+            item_need_update = item
+            break
+    if bool(item_need_update):
+        item_need_update.completed = True
+    return item_need_update
+
+def delete_todo_item(id: int) -> bool:
+    for index, item in enumerate(todos):
+        if item.id == id:
+            del todos[index]
+            return True
+    return False
+
